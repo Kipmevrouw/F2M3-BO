87869601d35bf343eec7f352c94ff9b321168a1f
Tijmen <30957@ma-web.nl>
2021-04-08 20:15:57 +0200

    grafiek

---
+++ js/grafiek.js
@@ -0,0 +1,37 @@
+const startGrafiek = () => {
+    // Hier komt de jouw code die na het laden van de pagina wordt uitgevoerd\
+    laadJSON("data.json");
+}
+
+const laadJSON = (url) => {
+   // Hier komt de code die gegevens uit een JSON bestand gaat laden
+   const aanvraag = new XMLHttpRequest();
+
+   // Omschrijf wat er moet gebeuren ALS je de data succesvol binnen hebt
+   aanvraag.onreadystatechange = () => {
+     if (aanvraag.readyState === 4 && aanvraag.status === 200) {
+       let jsonText = aanvraag.responseText;
+       console.log(jsonText);
+       data = JSON.parse (jsonText);
+
+       maakGrafiek(data);
+       // HIER KOMT CODE DIE UITGELEGD WORDT IN DE VIDEO
+     }
+   };
+ 
+   // serveraanvraag specificeren
+   aanvraag.open("GET", url, true);
+ 
+   // aanvraag versturen
+   aanvraag.send();
+ };
+
+
+const maakGrafiek = () => {
+    new Chartist.Line('#grafiek', data);
+
+}
+
+
+// Wacht tot de pagina is geladen, dan pas de startGrafiek functie uitvoeren
+window.addEventListener('DOMContentLoaded', startGrafiek);
