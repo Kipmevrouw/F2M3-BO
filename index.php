b192246e80749b0810dfc5cef11f2a2a96ab3396
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2021-04-15 19:57:48 +0200

    Ja

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <html lang="en">
 <head>
     <?php
@@ -14,6 +12,7 @@
     <link rel="stylesheet" href="css/style.css">
     <script src="js/apparaten.js"></script>
     <script src="js/page.js"></script>
+    <script src="js/weerlive.js"></script>
     <title>Duurzaamhuis</title>
     <link rel="stylesheet" href="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.css">
     <script src="//cdn.jsdelivr.net/chartist.js/latest/chartist.min.js"></script>
@@ -37,7 +36,24 @@
         </div></div>
         <br>
         <br>
-        <div class="item item5"><a class="weatherwidget-io" href="https://forecast7.com/nl/52d374d90/amsterdam/" data-label_1="Amsterdam" data-label_2="Weer" data-mode="Current" data-theme="blank" >AMSTERDAM</a></div>
+        <div class="item item5">
+            <div style="font-size: 40px; text-align: center;">
+                <p>Graden</p>
+            </div> 
+            <svg width="50%" height="50%" viewBox="0 0 42 42">
+                <circle cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="rgba(200,200,200,0.5)" stroke-width="1">
+        
+                </circle>
+                <circle cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="royalblue" stroke-width="1" stroke-dasharray="75 25" stroke-dashoffset="75" id="cirkelBoog">
+        
+                </circle>
+                <g class="temp">
+                    <text id="temperatuur" x="30%" y="54%">19</text>
+                    <text id="eenheid" x="52%" y="50%">&deg;C</text>
+                </g>
+            </svg>
+            <input type="range" min="1" max="30" step="1" value="18" id="thermostaat">
+    </div>
         <div class="item item6">
             <div class="item62">
             <div id="div1" style="padding:18px;float:left">
@@ -77,9 +93,9 @@
         <div class="item item7">
             <button id="item7aknop">Toon 7a</button>
             <button id="item7bknop">Toon 7b</button>
-            <div id="item7a" class="hidden">a</div>
-            <div id="item7b">
-                <!--<iframe src="http://30957.hosts1.ma-cloud.nl/F1M3/grafiek2/index.html" width="820px" height="620px" style="border:none;">-->
+            <div id="item7a" class="hidden">a
+            </div>
+            <div id="item7b">b
             </div>
         </div>
     </div>
@@ -87,5 +103,36 @@
 </body>
 <script>
     !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+    </script>
+    <script>
+        let temperatuur = 9;
+        const temperatuurUitvoer = document.getElementById('temperatuur');
+        const cirkelBoog         = document.getElementById('cirkelBoog');
+        const thermostaat         = document.getElementById('thermostaat');
+
+        // temperatuur = thermostaat.value;
+
+        const uitvoeren = (temp) => {
+            temperatuurUitvoer.innerHTML = temp;
+            let boog = temp * 100/31;
+            cirkelBoog.style.strokeDasharray = `${boog} ${100 - boog}`;
+        }
+         const animeerUitvoer = (temp) => {
+             let t = 0;
+             const timer = setInterval( () => {
+                 if( t <= temp ) {
+                     uitvoeren(t);
+                     t++;
+                 } else {
+                     clearInterval(timer);
+                 }
+             }, 25)
+         }
+
+        animeerUitvoer(thermostaat.value);
+        
+        thermostaat.addEventListener('change', () => {
+            animeerUitvoer(thermostaat.value)
+        })
     </script>
 </html>
